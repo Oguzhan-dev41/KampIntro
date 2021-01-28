bc3ec46ac81e2df79a90bf2f0b2689f80e220efe
Oğuzhan Kandar <oguzhankandar14@gmail.com>
2021-01-28 10:43:56 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım geliştirici yetiştirme kampı ";
+            string kurs2 = "Programlamaya başlangıç için kurs ";
+            string kurs3 = " java kursu";
+            string kurs4 = "Python";
+            string kurs5 = "c++ kursu";
+
+            //array - dizi
+            string[] kurslar = new string[] { "Yazılım geliştirici yetiştirme kampı " , "Programlamaya başlangıç için kurs ", " java kursu","Python" ,"c++ kursu"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("for bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu");
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming Convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi" + urun.Adi);
+
+        }
+
+        public void Ekle2(string UrunAdi,string aciklama,double fiyat,int StokAdedi)
+        {
+            Console.WriteLine("Tebrikler , Sepete eklendi" + urunAdi);
+
+        }
+
+
+
+    }
+}
